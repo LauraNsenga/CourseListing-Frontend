46596211813cd924eaff635a691c03bf411d9592
LauraNsenga <laura.nsenga@eagles.oc.edu>
2024-10-11 10:03:00 -0500

    routes

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,11 +1,21 @@
 // services/apiClient.js
 import axios from 'axios';
 
+var baseURL = "";
+if(import.meta.env.DEV)
+{
+  baseURL ="http://localhost/course/";
+
+}else{
+  baseurl ="/course-t9/";
+}
+
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3100/course', // Base URL for your backend endpoints
+  baseURL: 'http://localhost:3019/course', // Base URL for your backend endpoints
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
 
 export default apiClient;
