62806908d563dd86eb84821fde50d984a2b4a24b
LauraNsenga <laura.nsenga@eagles.oc.edu>
2024-10-11 14:45:58 -0500

    fixing port locations for BE

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3100/course', // Base URL for your backend endpoints
+  baseURL: 'http://localhost:3019/course-t9', // Base URL for your backend endpoints
   headers: {
     'Content-Type': 'application/json',
   },
